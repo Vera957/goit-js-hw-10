63079b482906891f82ea455361187431fd9e151d
Vera957 <good.answer.e@gmail.com>
2022-06-23 15:05:36 +0300

    css

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,10 @@
 import Notiflix from 'notiflix';
 
 const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
 export function fetchCountries(name) {
+  if (name === '') return;
   fetch(
     `https://restcountries.com/v2/name/${name.trim()}?fields=name,capital,population,flags,languages`
   )
@@ -13,6 +15,7 @@ export function fetchCountries(name) {
         if (response.status == '404') {
           Notiflix.Notify.failure('Oops, there is no country with that name');
           countryInfo.innerHTML = '';
+          countryList.innerHTML = '';
         }
       }
       return response.json();
@@ -39,14 +42,14 @@ function flagAndCountry(data) {
   const markUpSeveral = data
     .map(
       e =>
-        `<span class="flag" style="background-image: url(${e.flags.svg});
+        `<li class="countryList-item"><span class="flag" style="background-image: url(${e.flags.svg});
        width: 30px; height: 20px; display:block;
         background-position: contain; background-size: 30px 20px;
-        outline: 1px solid black;"></span><p>${e.name}</p>`
+        outline: 1px solid black;"></span><p>${e.name}</p></li>`
     )
     .join('');
-  console.log(markUpSeveral);
-  return (countryInfo.innerHTML = markUpSeveral);
+  countryInfo.innerHTML = '';
+  return (countryList.innerHTML = markUpSeveral);
 }
 
 function oneCountry(data) {
@@ -56,13 +59,16 @@ function oneCountry(data) {
         `
         <span class="flag" style="background-image: url(${e.flags.svg});
         width: 60px; height: 40px; display:block;
-        background-position: contain; background-size: 60px 40px;
+        background-position: cover; background-size: 60px 40px;
         outline: 1px solid black;"></span>
-        <h2>${e.name}</h2>
-        <p>Capital: ${e.capital}</p>
-        <p>Population: ${e.population}</p>
-        <p>Languages: ${e.languages.map(element => element.name)}</p>`
+        <p class="onlyCountryName">${e.name}</p>
+        <p class="onlyCountryInfo">Capital: ${e.capital}</p>
+        <p class="onlyCountryInfo">Population: ${e.population}</p>
+        <p class="onlyCountryInfo">Languages: ${e.languages
+          .map(element => element.name)
+          .join(', ')}</p>`
     )
     .join('');
+  countryList.innerHTML = '';
   return (countryInfo.innerHTML = markUp);
 }
