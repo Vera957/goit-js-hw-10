b3d9d555b10114e568e2d368d7ac62f3744f631d
Vera957 <good.answer.e@gmail.com>
2022-06-23 12:58:53 +0300

    lodash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-import { _, debounce } from 'lodash';
+import { _, debounce } from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
